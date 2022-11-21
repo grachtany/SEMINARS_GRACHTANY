47a91cb7a8a55e9ba13ed97e88e3cb662926754e
tatangracheva <grachtany@yandex.ru>
2022-11-21 21:44:20 +0300

    проверка квадрата

---
+++ task2/Program.cs
@@ -0,0 +1,15 @@
+﻿Console.Clear();
+Console.Write("Введите число a - ");
+int numberA = int.Parse(Console.ReadLine());
+Console.Write("Введите число b - ");
+int numberB = int.Parse(Console.ReadLine());
+Console.Write($"Является ли число {numberA} квадратом числа {numberB}? ");
+int square = numberB*numberB;
+if (numberA == square)
+{
+    Console.WriteLine ("Да, является.");
+}
+else
+{
+    Console.WriteLine ("Не является");
+}
