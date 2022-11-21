852890a5ea192635a4f2fdeb9caa61b501515712
tatangracheva <grachtany@yandex.ru>
2022-11-21 21:15:14 +0300

    Нахождение квадрата числа

--- task1/Program.cs
+++ task1/Program.cs
@@ -4,3 +4,4 @@ int numberA = int.Parse(Console.ReadLine());
 int square = numberA*numberA;
 Console.Write("Квадрат этого числа - ");
 Console.WriteLine(square);
+Console.Write($"Квадрат числа {numberA} равен {square}");
