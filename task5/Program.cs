0a895b2a2a72b5c42fbf96ba0b99f09a14a8d0a3
tatangracheva <grachtany@yandex.ru>
2022-11-21 22:50:50 +0300

    Остаток от деления

---
+++ task5/Program.cs
@@ -0,0 +1,5 @@
+﻿Console.Clear();
+Console.Write("Введите  трехзначное число - ");
+int number1 = int.Parse(Console.ReadLine());
+int result = number1%10;
+Console.Write($"Последняя цифра числа {number1} является {result} ");
