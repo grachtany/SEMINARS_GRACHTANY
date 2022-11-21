feefb43668718132e22c7da3be10fdd6bff0f581
tatangracheva <grachtany@yandex.ru>
2022-11-21 21:10:15 +0300

    Нахождение квадрата числа

---
+++ task1/Program.cs
@@ -0,0 +1,6 @@
+﻿Console.Clear();
+Console.Write("Введите число - ");
+int numberA = int.Parse(Console.ReadLine());
+int square = numberA*numberA;
+Console.Write("Квадрат этого числа - ");
+Console.WriteLine(square);
