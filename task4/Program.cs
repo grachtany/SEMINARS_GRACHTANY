c8da0997634509af20f7661dd49d69bfce81fc38
tatangracheva <grachtany@yandex.ru>
2022-11-21 22:36:28 +0300

    ряд чисел

---
+++ task4/Program.cs
@@ -0,0 +1,22 @@
+﻿Console.Clear();
+Console.Write("Введите число N ");
+int count=0;
+int number1 = int.Parse(Console.ReadLine());
+if (number1>0)
+    {
+    count = number1*(-1);
+        while (count<=number1)
+        {
+            Console.Write($"{count}, ");
+        count++;
+        }
+    }
+else 
+{
+    count = number1;
+    while (count<=number1*(-1))
+        {
+            Console.Write($" {count} , ");
+        count++;
+        }
+}
